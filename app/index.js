1b6a3ae26ac38389b308038b3113886267272024
Rajiv Tiwari <rajiv.tiwari@hsbc.com>
2017-03-02 18:17:34 +0000

    Updated for material ui integration
    Updated for material ui integration

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LoginComponent from './components/login';
 
-ReactDOM.render(<LoginComponent />, document.getElementById('app'));
+
+
+ReactDOM.render(<MuiThemeProvider><LoginComponent /></MuiThemeProvider>, document.getElementById('app'));
